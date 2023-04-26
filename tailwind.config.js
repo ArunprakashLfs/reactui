a418b3e23e278f6d7df68ac1c135b97b554bc889
kesavan <arunprakash7871@gmail.com>
2023-04-26 01:32:45 -0400

    Added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'blueColor': '#2a68ff',
+        'geryish': '#f1f4f8',
+        'CardShadow': '#f7f8f9',
+        'textColor': '#252b36',
+      }
+    },
   },
   plugins: [],
 }
