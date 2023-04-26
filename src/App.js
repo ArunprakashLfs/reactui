f493af344df4177b6eaf7135e6ab8410f96fdf41
kesavan <arunprakash7871@gmail.com>
2023-04-26 02:00:37 -0400

    imported components in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import Details from './components/Details/Details';
+import Footer from './components/Footer/Footer';
+import JobBar from './components/JobBar/JobBar';
 import NavBar from './components/NavBar/NavBar';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div >
       <NavBar/>
+      <Search/>
+      <JobBar/>
+      <Details/>
+      <Footer/>
     </div>
   );
 }
