7101c440b434e8d1755e35a7cf0909671b59dc01
kesavan <arunprakash7871@gmail.com>
2023-04-28 06:28:26 -0400

    created footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,23 @@
 const Footer = ()=>{
     return(
-        <div>Footer</div>
+        <div className="bg-blueColor grid grid-cols-5 justify-center items-center mt-[4rem] p-[5rem] gap-[3rem]">
+            <div>
+                <div className="logoDiv">
+                    <h1 className="logo text-[25px] text-white"><strong>Job</strong>Search</h1>
+                </div>
+                <p className="text-white font-semibold opacity-[.7] hover:opacity-[1]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, culpa!</p>
+            </div>
+            <div className="grid text-white">
+                <h2>Company</h2>
+                <div>
+                    <li>value</li>
+                    <li>Worklife</li>
+                    <li>carrier</li>
+                    <li>workflow</li>
+                </div>
+            </div>
+
+        </div>
     )
 }
 export default Footer;
